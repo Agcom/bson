d03a62d9ea4fedacd86161462b6def2a65ea4a5a
Alireza Ghasemi <agcombest@gmail.com>
2020-08-12 20:03:50 +0430

    Added `codec-registry` module
    Added `BsonValueTransformingCodec`

---
+++ codec-registry/src/main/kotlin/com/github/agcom/bson/codecs/BsonValueTransformingCodec.kt
@@ -0,0 +1,28 @@
+package com.github.agcom.bson.codecs
+
+import org.bson.BsonReader
+import org.bson.BsonValue
+import org.bson.BsonWriter
+import org.bson.codecs.BsonValueCodec
+import org.bson.codecs.Codec
+import org.bson.codecs.DecoderContext
+import org.bson.codecs.EncoderContext
+
+abstract class BsonValueTransformingCodec<T> : Codec<T> {
+
+    companion object {
+        val bsonCodec: Codec<BsonValue> = BsonValueCodec()
+    }
+
+    final override fun encode(writer: BsonWriter, value: T, encoderContext: EncoderContext) {
+        bsonCodec.encode(writer, toBson(value), encoderContext)
+    }
+
+    final override fun decode(reader: BsonReader, decoderContext: DecoderContext): T {
+        return fromBson(bsonCodec.decode(reader, decoderContext))
+    }
+
+    abstract fun toBson(value: T): BsonValue
+    abstract fun fromBson(value: BsonValue): T
+
+}
