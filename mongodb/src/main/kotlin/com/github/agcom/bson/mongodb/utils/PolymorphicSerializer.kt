eedeb30ddb0b0beb3878a79cb13754d0278e43fa
Alireza Ghasemi <agcombest@gmail.com>
2020-08-31 12:47:18 +0430

    Refactored `mongodb` main module

--- mongodb/src/main/kotlin/com/github/agcom/bson/mongodb/utils/PolymorphicSerializer.kt
+++ mongodb/src/main/kotlin/com/github/agcom/bson/mongodb/utils/PolymorphicSerializer.kt
@@ -8,13 +8,17 @@ import kotlin.reflect.KClass
 
 /**
  * Iterates on all of the registered polymorphic serializers.
+ *
  * Won't fail (returns a non-null [PolymorphicSerializer]) if one of the below conditions hold,
  * - A serializer for the [kClass] is registered as a subclass (safe serializer)
  * - [kClass] is registered as a base class (unsafe serializer)
+ *
  * The serializer yielded from the second condition is unsafe, because there is no guarantee that all sub-classes of [kClass] are present.
  * @return A [PolymorphicSerializer] for the [kClass] or null if no polymorphic serializer related to the [kClass] is registered.
+ * @param T The base class
  */
-internal fun <T : Any> SerialModule.getPolymorphic(kClass: KClass<T>): PolymorphicSerializer<T>? {
+@Suppress("UNCHECKED_CAST")
+internal fun <T : Any> SerialModule.getPolymorphic(kClass: KClass<out T>): PolymorphicSerializer<T>? {
     lateinit var baseClazz: KClass<T>
     try {
         dumpTo(object : SerialModuleCollector {
@@ -22,7 +26,6 @@ internal fun <T : Any> SerialModule.getPolymorphic(kClass: KClass<T>): Polymorph
                 // No op
             }
 
-            @Suppress("UNCHECKED_CAST")
             override fun <Base : Any, Sub : Base> polymorphic(
                 baseClass: KClass<Base>,
                 actualClass: KClass<Sub>,
@@ -30,14 +33,14 @@ internal fun <T : Any> SerialModule.getPolymorphic(kClass: KClass<T>): Polymorph
             ) {
                 if (kClass == actualClass || kClass == baseClass) {
                     baseClazz = baseClass as KClass<T>
-                    throw SerializerFoundException
+                    throw BaseClassFound
                 }
             }
         })
-    } catch (ex: SerializerFoundException) {
+    } catch (ex: BaseClassFound) {
         return PolymorphicSerializer(baseClazz)
     }
     return null
 }
 
-private object SerializerFoundException : RuntimeException("Stop dumping, already found the needed serializer")
+private object BaseClassFound : RuntimeException("Stop dumping, already found the needed base class")
